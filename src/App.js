d447f3b026a2d1e5e27389b8f18cf3e3b4ae840a
priyank <patelpriyank3112002@gmail.com>
2023-05-11 15:49:51 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Usestate from "./hooks/Usestate";
 import Usestate1 from "./hooks/Usestate1";
 import "./App.css"
+import Usereducer from "./hooks/Usereducer";
+import Usereducer1 from "./hooks/Usereducer1";
 
 function App() {
   return (
    <div>
     < Usestate />
     < Usestate1 />
+    <Usereducer />
+    <Usereducer1 />
    </div>
       
   );
